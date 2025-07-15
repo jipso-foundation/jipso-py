972f312e45e1d46a0aec829f3582f370b7f16677
{{HOST}} <{{HOST}}@{{DOMAIN}}>
2025-07-15 10:46:06 +0700

    Release version v0.1.11

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'JIPSO'
 author = 'JIPSO Foundation'
-release = '0.1.10'
+release = '0.1.11'
 
 # -- General configuration ---------------------------------------------------
 
--- jipso/cli.py
+++ jipso/cli.py
@@ -2,7 +2,7 @@ import typer
 from typing import Optional
 
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 cli = typer.Typer(
   name = 'jipso',
